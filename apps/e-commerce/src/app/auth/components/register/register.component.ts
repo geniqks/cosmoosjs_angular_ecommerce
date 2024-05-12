1f6b03f733625281b4545afa729255e720bc4fb4
Mathys <mathystheolade973@gmail.com>
2024-05-13 01:10:37 +0200

    [front] fix checkbox labels, username exist

--- apps/e-commerce/src/app/auth/components/register/register.component.ts
+++ apps/e-commerce/src/app/auth/components/register/register.component.ts
@@ -61,7 +61,9 @@ export class RegisterComponent implements OnInit {
         console.log('response', response)
         console.log(this.form.get('username')?.errors);
         if (response) {
-          this.form.get('username')?.setErrors({ usernameExist: true });
+          this.form.get('username')?.setErrors({ 'usernameExist': true });
+        } else {
+          this.form.get('username')?.setErrors(null);
         }
         this.form.updateValueAndValidity();
       });
