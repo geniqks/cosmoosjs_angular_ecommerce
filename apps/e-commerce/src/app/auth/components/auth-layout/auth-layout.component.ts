40698f044896367598e87a52c137978f7be83bee
Mathys <mathystheolade973@gmail.com>
2024-05-13 21:02:23 +0200

    remove encapsulation

--- apps/e-commerce/src/app/auth/components/auth-layout/auth-layout.component.ts
+++ apps/e-commerce/src/app/auth/components/auth-layout/auth-layout.component.ts
@@ -1,9 +1,8 @@
-import { Component, ViewEncapsulation } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-auth-layout',
   templateUrl: './auth-layout.component.html',
   styleUrls: ['./auth-layout.component.scss'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class AuthLayoutComponent { }
